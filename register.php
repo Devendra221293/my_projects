b83a3e0ea2b5271ca8346c12b5af28ec9530ae11
Devendra221293 <devenkulkarni221293@gmail.com>
2017-03-02 15:05:04 +0530

    Update register.php

--- register.php
+++ register.php
@@ -4,6 +4,7 @@ if($_SESSION['email']) {
 //if sesssion exist then load the page.
 ?>
 <!DOCTYPE html>
+<html>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/1000hz-bootstrap-validator/0.11.5/validator.js"></script>
@@ -12,16 +13,16 @@ if($_SESSION['email']) {
 
 <script>
 		$(document).ready(function() {
-    
-    $('#update').hide();
-		$('#cancel').hide();
+                //hide update button
+   		$('#update').hide();
+		//$('#cancel').hide();
 		$('#update_heading').hide();
 			
    //code for initcap
    		
    $('#id_name,#id_middle_name,#id_last_name,#id_address,#id_city').on('keydown', function(event) {
        if (this.selectionStart == 0 && event.keyCode >= 65 && event.keyCode <= 90 && !(event.shiftKey) && !(event.ctrlKey) && !(event.metaKey) && !(event.altKey)) {
-	            var $t = $(this);
+	            	var $t = $(this);
 	           	event.preventDefault();
 	           	var char = String.fromCharCode(event.keyCode);
 	           	$t.val(char + $t.val().slice(this.selectionEnd));
@@ -29,10 +30,9 @@ if($_SESSION['email']) {
 	        	}
 	    		});
 			
-	 //get customer id for edit details
+	 		//get customer id for edit details
 			<?php
 			$customer_id=$_GET['customer_id'];
-			// $flag=$_GET['flag'];	
 			?>
       
   //here if wants to edit the user,this fill the data into the fields
@@ -45,11 +45,11 @@ if($_SESSION['email']) {
 			
 				$('#save').hide();
 				$('#update').show();
-				$('#cancel').show();
+				//$('#cancel').show();
 			
 				$.ajax({
 					type:"POST", 
-	        url: "process_customer.php",
+	        			url: "process_customer.php",
 					data:'customer_id='+customer_id,
 					success: function(result){
 						
@@ -57,9 +57,9 @@ if($_SESSION['email']) {
 					
 						$("#id_username").val(data[0]);
 						$("#id_name").val(data[3]);
-            $("#id_last_name").val(data[4]);
+            					$("#id_last_name").val(data[4]);
 						$("#id_email").val(data[5]);
-            $("#id_contact").val(data[6]);
+            					$("#id_contact").val(data[6]);
 						$("#id_city").val(data[7]);
 					}
 		
@@ -70,20 +70,20 @@ if($_SESSION['email']) {
 		//when submit  button is clicked  to submit customer info
         		$('#save').click(function()
 			{
-            $('#id_customer').val(customer_id);
-            var username         = $('#id_user_name').val();
-	          var new_password     = $('#id_new_password').val();
-		        var confirm_password = $('#id_confirm_password').val();
-						var name             = $('#id_name').val();
-						var last_name        = $('#id_last_name').val();
-						var city             = $('#id_city').val();
-						var contact_number   = $('#id_contact').val();
-						var email            = $('#id_email').val();
+            				$('#id_customer').val(customer_id);
+           				var username         = $('#id_user_name').val();
+	          			var new_password     = $('#id_new_password').val();
+		        		var confirm_password = $('#id_confirm_password').val();
+					var name             = $('#id_name').val();
+					var last_name        = $('#id_last_name').val();
+					var email            = $('#id_email').val();
+					var contact_number   = $('#id_contact').val();
+					var city             = $('#id_city').val();
 					
-						//prevent form if fields are empty
+					//prevent form if fields are empty
 						
-						if(name!="" && last_name!="" )
-			  			{
+					if(name!="" && last_name!="" )
+			  		{
 				
 			//prevent form if fields are empty
 		
@@ -98,12 +98,12 @@ if($_SESSION['email']) {
 					{
 						$('#id_user_name').val("");
 						$('#id_new_password').val("");
-            $('#id_confirm_password').val("");
-            $('#id_name').val("");
+            					$('#id_confirm_password').val("");
+            					$('#id_name').val("");
 						$('#id_last_name').val("");
 						$('#id_email').val("");
 						$('#id_contact').val("");
-            $('#id_city').val("");
+            					$('#id_city').val("");
 					
 						//show modal
 			 			$("#getCodeModal").modal('show');
@@ -113,13 +113,13 @@ if($_SESSION['email']) {
         						$(this).find('#ok_btn').focus();
     						});
     						
-    				//reload on modal close	
+    						//reload on modal close	
 			 			$('#getCodeModal').on('hidden.bs.modal', function () {
   						location.reload();
 						});
 						
 			 			
-				 }//success close
+				       }//success close
 					        
 				});
 		 	  }else{
@@ -134,7 +134,7 @@ if($_SESSION['email']) {
 					{
 	
 						$('#id_customer').val(customer_id);
-            var user_name      = $('#id_user_name').val();
+            					var user_name      = $('#id_user_name').val();
 						var name           = $('#id_name').val();
 						var last_name      = $('#id_last_name').val();
 						var email          = $('#id_email').val();
@@ -145,7 +145,7 @@ if($_SESSION['email']) {
 				//prevent form if fields are empty
 		  				if(name!="" && last_name!="" )
 		  				{
-			var detail='&username='+username+'&name='+name+'&last_name='+last_name+'&email='+email+'&contact_number='+contact_number+'&city='+city+;
+			var detail='&username='+user_name+'&name='+name+'&last_name='+last_name+'&email='+email+'&contact_number='+contact_number+'&city='+city+;
 			
 						$.ajax
 						({
@@ -170,12 +170,22 @@ if($_SESSION['email']) {
 					//do nothing
 			      		}
 		  
-            });//click ends here	
+                               });//click ends here
+			
+			$('#cancel').click(function()
+			{
+			     $('#id_user_name').val("");
+			     $('#id_new_password').val("");
+            		     $('#id_confirm_password').val("");
+            		     $('#id_name').val("");
+			     $('#id_last_name').val("");
+			     $('#id_email').val("");
+			     $('#id_contact').val("");
+            		     $('#id_city').val("");
+
+			});//click ends here
 	
-	
-	
-   
- 		 });//ready close
+ 		      });//ready close
    
      
 </script>
@@ -189,62 +199,58 @@ if($_SESSION['email']) {
          <h3 class="title">Update Information</h3> 
 </div>
                             
-<div class="title-block" id="registered_message" style="display:none;" >
-         <h3 class="title" style="text-align:center;color:green">Customer has been registerd successfully</h3> 
-</div>
-                            
 <form name="register_form" id="register_form"  data-toggle="validator" role="form">
 
-<fieldset class="form-group col-md-6">
+<div class="form-group col-md-6">
            <label for="inputname" class="control-label">Username</label><span class="label"style="Color:red;">*</span>      	
                  <input type="text" onkeypress='return event.charCode >= 65 && event.charCode <= 90 || event.charCode == 32 || event.charCode == 39 || event.charCode >= 97 && event.charCode <= 122 || event.charCode <= 8' class="form-control crc-textfield"  pattern="^[a-zA-Z ']+$" name="user_name" id="id_user_name"  placeholder="" value="" required>
-						<div class="help-block with-errors"></div>
-</fieldset>
+	   <div class="help-block with-errors"></div>
+</div>
 
-<fieldset class="form-group">
+<div class="form-group">
           <label for="inputStreet" class="control-label"> Password</label><span class="label"style="Color:red;">*</span>
                  <input type="password" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,10}" class="form-control" name="new_password" id="id_new_password" placeholder="Minimum 8 characters" value="" data-error="Password must contain 1 Uppercase Alphabet, 1 Lowercase Alphabet, 1 Number and 1 Special Character " required/>
           <div class="help-block with-errors"></div>
-</fieldset>   
+</div>   
 
-<fieldset class="form-group">
+<div class="form-group">
           <label for="inputStreet" class="control-label">Confirm Password</label><span class="label"style="Color:red;">*</span>
                  <input type="password" class="form-control" name="confirm_password" id="id_confirm_password" placeholder="" value="" required/>
-					<div class="help-block with-errors"></div>
-</fieldset>
+	  <div class="help-block with-errors"></div>
+</div>
 
-<fieldset class="form-group col-md-6">
+<div class="form-group col-md-6">
            <label for="inputname" class="control-label">First Name</label><span class="label"style="Color:red;">*</span>      	
                  <input type="text" onkeypress='return event.charCode >= 65 && event.charCode <= 90 || event.charCode == 32 || event.charCode == 39 || event.charCode >= 97 && event.charCode <= 122 || event.charCode <= 8' class="form-control crc-textfield"  pattern="^[a-zA-Z ']+$" name="n_name" id="id_name"  placeholder="" value="" required>
-						<div class="help-block with-errors"></div>
-</fieldset>
+	   <div class="help-block with-errors"></div>
+</div>
 
 
-<fieldset class="form-group col-md-6">
+<div class="form-group col-md-6">
            <label for="inputsurname" class="control-label">Last Name</label><span class="label"style="Color:red;">*</span>
                    <input type="text" onkeypress='return event.charCode >= 65 && event.charCode <= 90 || event.charCode == 32 || event.charCode == 39 || event.charCode >= 97 && event.charCode <= 122 || event.charCode <= 8' pattern="^[a-zA-Z ']+$" class="form-control crc-textfield" name="n_last_name" id="id_last_name" placeholder="" value="" required>
-			      <div class="help-block with-errors"></div>
-</fieldset>
+	   <div class="help-block with-errors"></div>
+</div>
 
-<fieldset class="form-group col-md-6">
+<div class="form-group col-md-6">
            <label for="inputEmail" class="control-label"> Email</label>
                    <input type="email" class="form-control crc-textfield" name="n_email" id="id_email"   placeholder="" value="">
-    			 <div class="help-block with-errors"></div>
-</fieldset>
+    	   <div class="help-block with-errors"></div>
+</div>
                         
                         
-<fieldset class="form-group col-md-6">
-          <label for="inputphone" class="control-label">Contact Number</label></span>
+<div class="form-group col-md-6">
+          <label for="inputphone" class="control-label">Contact Number</label>
                   <input type="text" onkeypress='return event.charCode >= 48 && event.charCode <= 57 || event.charCode == 46 || event.charCode <= 8' pattern="[0-9]*{10}" minlength="10" maxlength="10" class="form-control" name="n_contact" id="id_contact" placeholder="" value="" data-error="Contact Number must be 10 digit. "/>
-				  <div class="help-block with-errors"></div>
-</fieldset>
+	  <div class="help-block with-errors"></div>
+</div>
                         
 
-<fieldset class="form-group col-md-6">
+<div class="form-group col-md-6">
           <label for="inputCity" class="control-label">Location</label>
                  <input type="text" pattern="^[a-zA-Z ']+$" onkeypress='return event.charCode >= 65 && event.charCode <= 90 || event.charCode == 32 || event.charCode == 39 || event.charCode >= 97 && event.charCode <= 122 || event.charCode <= 8' class="form-control" name="n_city" id="id_city"  placeholder="" value=""><div class="help-block with-errors"></div>
           <div class="help-block with-errors"></div>
-</fieldset>
+</div>
 
 
 <div class="form-group">
@@ -252,7 +258,7 @@ if($_SESSION['email']) {
                      			
            <input id="update" type="button" value="Update" class="btn btn-primary"  style="width:30%;  margin-left:2%;"/>
                      			
-           <input id="cancel" type="button" value="Reset" class="btn btn-primary" onClick="javascript:history.back();"  style="width:30%"/>		
+           <input id="cancel" type="button" value="Reset" class="btn btn-primary"  style="width:30%"/>		
 </div>
 
 
@@ -279,8 +285,8 @@ if($_SESSION['email']) {
 				  </div>
 				     
 				     
-				     		<!-- Modal 2 -->
- 							<div class="modal fade" id="updateModal" style="margin-top:3%;" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+				 <!-- Modal 2 -->
+ 			     <div class="modal fade" id="updateModal" style="margin-top:3%;" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
    				<div class="modal-dialog modal-md">
       					<div class="modal-content">
        						<div class="modal-header">
