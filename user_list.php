090d347830dc1516d795b14b8bea0dc63f036757
Devendra221293 <devenkulkarni221293@gmail.com>
2017-03-02 15:29:32 +0530

    Update user_list.php

--- user_list.php
+++ user_list.php
@@ -17,35 +17,35 @@ if($_SESSION['email']) {
 			  url:"process_search.php",
 			  success: function(result)
 			  {	  
-               var data = $.parseJSON(result);
+               		  var data = $.parseJSON(result);
                                           
-               console.log(data);
-               console.log(data[0].name);
+               		  console.log(data);
+                          console.log(data[0].name);
                                       
-               // alert(data[0].name);
+               		 // alert(data[0].name);
                
-               //code to clean table onload
-               $('#mytable').html("");
+               		//code to clean table onload
+               		$('#mytable').html("");
 						
-			         var rows = "";
-			         var head ="";
+		        var rows = "";
+		        var head ="";
 			     
-           //create table head and rows dynamicaly
-			     head +="<tr><th style='width:25%;'>Customer Name</th><th style='width:30%;'>Address</th><th style='width:25%;'>Email</th><th style='width:15%;'>Contact No</th><th style='width:8%;'>Action</th></tr>";
+                        //create table head and rows dynamicaly
+			head +="<tr><th style='width:25%;'>Customer Name</th><th style='width:30%;'>Address</th><th style='width:25%;'>Email</th><th style='width:15%;'>Contact No</th><th style='width:8%;'>Action</th></tr>";
 			     
-			     $.each(data,function(index){
+			$.each(data,function(index){
 					  
 			     rows +="<tr><td>" + data[index].name +" "+  data[index].last_name + "</td><td>" + data[index].address+ "</td><td>" + data[index].primary_email+ "</td><td>" + data[index].primary_contact+ "</td><td><a href='http://www.corecotechnologies.com/roonak-crm/dashboard.php?customer_id="+ data[index].customer_id +"'><button type='button' id='editbtn' name='Edit' class='editbtn btn btn-primary glyphicon glyphicon-pencil' title='Edit'><span style='display:none;'>Edit</button></a></td></tr>";
 						
-			     });
+			});
 				
-          //table head append
-				  $(head).appendTo("#mytable");
-				  //data row append
-				  $(rows).appendTo("#mytable");
+                        //table head append
+		       $(head).appendTo("#mytable");
+			//data row append
+		       $(rows).appendTo("#mytable");
 					 
-			      } 
-			   });
+		     } 
+                 });
 	    });	
 </script>
        <table align="center" id="mytable" border="1px" class="table table-bordered table-hover table-responsive">
